2aa68a5bc2f30f48affc9d92a23d96aab1db7224
Judy Sun <ccffsun@gmail.com>
2020-04-05 00:00:20 -0700

    picker for date

---
+++ PickerForDate/app/src/main/java/com/example/pickerfordate/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pickerfordate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment =  new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+}
