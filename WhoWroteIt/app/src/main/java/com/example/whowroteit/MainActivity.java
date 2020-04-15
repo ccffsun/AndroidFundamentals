a788037054cae426e4bdf8158578b11eda58ba1a
Judy Sun <ccffsun@gmail.com>
2020-04-14 22:35:20 -0700

    who wrote it AsyncTask

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.java
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.whowroteit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -38,6 +40,28 @@ public class MainActivity extends AppCompatActivity {
         if(inputManager != null) {
             inputManager.hideSoftInputFromWindow(view.getWindowToken(),
                     InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if(connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        if(networkInfo != null && networkInfo.isConnected()
+                && queryString.length() !=0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        }else{
+            if(queryString.length()==0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            }else{
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
         }
     }
 }
