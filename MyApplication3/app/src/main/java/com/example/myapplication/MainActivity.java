841c61deca5f67b4bda9989a70b273f027eee37e
Judy Sun <ccffsun@gmail.com>
2020-04-25 12:31:13 -0700

    androidshp

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        myViewModel =  ViewModelProviders.of(this, new SavedStateViewModelFactory(getApplication(),this)).get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
+    }
+}
---
+++ MyApplication3/app/src/main/java/com/example/myapplication/MyViewModel.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.SavedStateHandle;
+
+import com.example.myapplication.R;
+
+public class MyViewModel extends AndroidViewModel {
+
+    SavedStateHandle handle;
+    String key = getApplication().getResources().getString(R.string.data_key);
+    String shpName = getApplication().getResources().getString(R.string.shpName);
+
+    public MyViewModel(@NonNull Application application, SavedStateHandle handle) {
+        super(application);
+        this.handle = handle;
+        if(!handle.contains(key)){
+            load();
+        }
+    }
+    public LiveData<Integer> getNumber(){
+        return handle.getLiveData(key);
+    }
+
+    void load(){
+        SharedPreferences shp = getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
+        int x = shp.getInt(key,0);
+        handle.set(key,x);
+    }
+
+    void save() {
+        SharedPreferences shp = getApplication().getSharedPreferences(shpName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shp.edit();
+        editor.putInt(key,getNumber().getValue());
+        editor.apply();
+    }
+
+    public void add(int x) {
+        handle.set(key,getNumber().getValue()+x);
+        save();
+    }
+}
