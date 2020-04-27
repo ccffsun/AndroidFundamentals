4e8cbae4e981616521ae517c8582119f71ddf891
Judy Sun <ccffsun@gmail.com>
2020-04-27 11:53:42 -0700

    myNavigation2

---
+++ MyNavigation2/app/src/main/java/com/example/mynavigation2/DetailFra.java
@@ -0,0 +1,39 @@
+package com.example.mynavigation2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFra extends Fragment {
+
+    public DetailFra() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String string = getArguments().getString("name_key");
+        String string2 =getArguments().getString("my_name");
+        TextView textView = getView().findViewById(R.id.textView);
+        textView.setText(string2);
+    }
+}
