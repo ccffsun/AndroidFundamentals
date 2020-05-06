b42213830d1deededb7afee24d36f5d7a81b66d2
Judy Sun <ccffsun@gmail.com>
2020-05-05 23:04:12 -0700

    recycler view

--- RoomBasic/app/src/main/java/com/example/roombasic/MainActivity.java
+++ RoomBasic/app/src/main/java/com/example/roombasic/MainActivity.java
@@ -1,57 +1,98 @@
 package com.example.roombasic;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.room.Delete;
-import androidx.room.Room;
-import androidx.room.Update;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonInsert, buttonUpdate, buttonClear, buttonDelete;
-    TextView textView;
     WordViewModel wordViewModel;
+    RecyclerView recyclerView;
+    Switch aSwitch;
+    MyAdapter myAdapter1,myAdapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        myAdapter1 = new MyAdapter(false);
+        myAdapter2 = new MyAdapter(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter1);
+        aSwitch = findViewById(R.id.switch2);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    recyclerView.setAdapter((myAdapter2));
+                }else {
+                    recyclerView.setAdapter(myAdapter1);
+                }
+            }
+        });
+
         wordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
-        textView = findViewById(R.id.textView);
+
         wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
             @Override
             public void onChanged(List<Word> words) {
-                StringBuilder text = new StringBuilder();
-                for (int i = 0; i < words.size(); i++) {
-                    Word word = words.get(i);
-                    text.append(word.getId()).append(":").append(word.getWords()).append("=").append(word.getChineseMeaning()).append("\n");
+                myAdapter1.setAllWords(words);
+                myAdapter1.notifyDataSetChanged();
+                myAdapter2.setAllWords(words);
+                myAdapter2.notifyDataSetChanged();
                 }
-                textView.setText(text.toString());
-
-            }
         });
 
         buttonInsert = findViewById(R.id.buttonInsert);
         buttonClear = findViewById(R.id.buttonClear);
-        buttonDelete = findViewById(R.id.buttonDelete);
-        buttonUpdate = findViewById(R.id.buttonUpdate);
+
 
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Word word1 = new Word("Hello", "你好");
-                Word word2 = new Word("World", "世界");
-                wordViewModel.insertWords(word1,word2);
+                String[] english ={
+                        "Hello",
+                        "World",
+                        "Android",
+                        "Google",
+                        "Studio",
+                        "Project",
+                        "Database",
+                        "Recycler",
+                        "View",
+                        "String",
+                        "Value",
+                        "Integer"
+                };
+                String[] chinese = {
+                        "你好",
+                        "世界",
+                        "安卓系统",
+                        "谷歌公司",
+                        "工作室",
+                        "项目",
+                        "数据库",
+                        "回收站",
+                        "视图",
+                        "字符串",
+                        "价值",
+                        "整数类型"
+                };
+                for(int i = 0;i <english.length;i++) {
+                    wordViewModel.insertWords(new Word(english[i],chinese[i]));
+                }
             }
         });
 
@@ -59,24 +100,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
             wordViewModel.DeleteAllWords();
-            }
-        });
-
-        buttonUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Word word = new Word("Hi", "你好啊");
-                word.setId(39);
-                wordViewModel.UpdateWords(word);
-            }
-        });
-
-        buttonDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Word word = new Word("Hello", "world");
-                word.setId(40);
-                wordViewModel.DeleteWords(word);
             }
         });
     }
